f0652886049e55f5258c4b1ad719e561e7ce5eee
Iryna Shandar <iryna.shandar@gmail.com>
2022-06-19 13:25:25 +0300

    work with photo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
+import React from 'react';
 import Header from './Components/HeaderSection';
 import Footer from './Components/FooterSection';
+import Items from './Components/Items';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Avocado',
+          img: 'avocado.jpg',
+          desc: 'Lorem ...',
+          category: '',
+          price: '59.45',
+        },
+        {
+          id: 1,
+          title: 'Lemon',
+          img: 'lemon.jpg',
+          desc: 'Lorem ...',
+          category: '',
+          price: '59.45',
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
