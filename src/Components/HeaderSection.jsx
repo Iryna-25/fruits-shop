f424087ed4e10df942b8be781a203a86f5aad9a7
Iryna Shandar <iryna.shandar@gmail.com>
2022-07-07 14:18:46 +0300

    header

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -3,10 +3,13 @@ import { FaShoppingCart } from "react-icons/fa";
 import Order from "./Order";
 
 const showOders = (props) => {
+    let summa = 0
+    props.orders.forEach(el => summa += Number.parseFloat(el.price) )
     return (<div>
         {props.orders.map(el => (
-            <Order key={el.id} item={el} />
+            <Order onDelete={props.onDelete} key={el.id} item={el} />
         ))}
+        <p className="sum">Summa: {new Intl.NumberFormat().format(summa)}</p>
     </div>
     )
 }
